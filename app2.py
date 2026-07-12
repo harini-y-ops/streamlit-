81f506c9e4218e660d3896c83001eda77a472d29
harini-y-ops <harini.n20061@gmail.com>
2026-07-12 19:46:33 +0530

    Update app2.py
    The Multiverse of Chatbots** is an interactive AI chatbot application built using **Streamlit** and the **Google Gemini API**. Users can select a fictional character, adjust the personality intensity using a slider, and chat with the AI, which responds while staying in character. The app combines prompt engineering with a simple, user-friendly interface to create an engaging role-playing experience.

--- app2.py
+++ app2.py
@@ -1,9 +1,30 @@
 import streamlit as st 
-st.title('THE MULTIVERSE OF CHATBOTS')
 personality=st.sidebar.selectbox("Who do you want to talk to?",[
   "Hermione Granger","Sheldon Cooper","Mad Max","Voldemort","Doraemon"
 ])
+avatars = {
+  "Hermione Granger": "📚",
+  "Sheldon Cooper" : "🤓",
+  "Mad Max" : "🚗",
+  "Voldemort" : "🐍",
+  "Doraemon": "🐱"
+}
+avatar = avatars[personality]
+
+st.title(f"{avatar}THE MULTIVERSE OF CHATBOTS")
+
+
 intensity=st.sidebar.slider("INTENSITY",min_value=1,max_value=100)
+if intensity <=20:
+  intensity_prompt="Be very subtle and occasionally show character"
+elif intensity <=40:
+  intensity_prompt="Stay mostly in character and quote a few lines"
+elif intensity<=60:
+  intensity_prompt="Clearly act like the character and make it realistic"
+elif intensity<=80:
+  intensity_prompt="Strongly act like the character and Use their catchphrases, mannerisms, and speaking style."
+else:
+  intensity_prompt="Fully become the character. Never break character. Use their emotions, vocabulary, humor, and personality to the maximum."
 from google import genai
 import os 
 from dotenv import load_dotenv 
@@ -12,7 +33,15 @@ client=genai.Client(api_key= os.getenv("GEMINI_API_KEY"))
 user_message=st.text_input("Enter anything: ")
 if st.button("SEND"):
   if user_message:
-    ai_instructions=f"You are acting as {personality}.Respond to the message sent by the user,staying completely in character:{user_message}."
+    ai_instructions=f""" You are {personality}.
+     Character Intensity = {intensity}/100
+     Instructions:
+{intensity_prompt}
+
+Respond to the user's message while staying completely in character.
+
+User: {user_message}
+"""
     with st.spinner("CONNECTING TO THE MULTIVERSE....."):
       response=client.models.generate_content(
         model="gemini-2.5-flash",
@@ -23,6 +52,7 @@ if st.button("SEND"):
       st.write(response.text)
   else:
     st.warning("Please type a message first")
+      
       
 
 
